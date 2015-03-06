86a865b1a4efa0d78e2c79a5d53335fa037b3122
Tim Kaufeldt <tim.kaufeldt@gmail.com>
2015-03-06 19:24:08 +0000

    array definitions

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,21 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function reversePlusOne (argumentArray) {
+  argumentArray.reverse();
+  argumentArray.unshift(1);
+  return(argumentArray);
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function plusesEverywhere (plusArray) {
+  // var lastEntry = (plusArray.length - 1);
+  // var counter = 0;
+  // if (counter < plusArray.length) 
+  // {
+  //  return(plusArray[counter] + "+");
+  //  counter++;
+  //}
+  return(plusArray.join("+"));
 
-module.exports.arrayQuantityPlusOne = undefined;
+};
+
+module.exports.arrayQuantityPlusOne = function arrayQuantityPlusOne (array) {
+  return(array.length + 1);
+};
