1f6dc2161ca05c040fbc14f0f4d68411fa0e6c6d
Tim Kaufeldt <tim.kaufeldt@gmail.com>
2015-03-06 20:07:39 +0000

    grocery list

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,17 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function addItem (newItem, newArray) {     
+  if (!(newArray.indexOf(newItem) > -1)) 
+      {
+        newArray.push(newItem);
+        return(newArray);
+      }
+      else
+        { 
+          return(newArray);
+        }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function reverseSortedList (newArray) {
+  newArray.sort();
+  newArray.reverse();
+  return(newArray);
+};
