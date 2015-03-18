f233d621698068e25541cac04b1cce49b239f8c9
Tim Kaufeldt <tim.kaufeldt@gmail.com>
2015-03-18 02:12:51 +0000

    loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,24 @@
-module.exports.stream = undefined;
+module.exports.stream = function stream (conditionalFn, actionFn) 
+{
+ var trueValue = (conditionalFn());
+ while (trueValue != false)
+ {
+  actionFn();
+  trueValue = conditionalFn();
+ }
 
-module.exports.sumNumbers = undefined;
+};
+
+module.exports.sumNumbers = function sumNumbers (argArray) 
+{
+  var counter = (argArray.length - 1);
+  var sumNum = 0;
+  while (counter >= 0)
+  {
+    newNum = (sumNum + argArray[counter]);
+    sumNum = newNum;
+    counter--;
+  }
+  return(sumNum);
+};
 
